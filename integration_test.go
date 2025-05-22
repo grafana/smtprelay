77674cf0d74c9840a200ef4151ab107464436ddb
Dave Henderson <dave.henderson@grafana.com>
2025-05-22 16:26:19 -0400

    fix(lint): Enable intrange linter
    Signed-off-by: Dave Henderson <dave.henderson@grafana.com>

--- integration_test.go
+++ integration_test.go
@@ -110,7 +110,7 @@ func startRelay(ctx context.Context, t *testing.T, srvAddr string) string {
 	}()
 
 	// wait for the server to start
-	for n := 0; n < 10; n++ {
+	for range 10 {
 		conn, err := net.Dial("tcp", addr)
 		if err == nil {
 			_ = conn.Close()
